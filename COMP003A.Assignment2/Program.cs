1d4ec492a37a91a2abe7d76ec4706463c5daf052
aaronhdcomps <aaron.hdcomps@gmail.com>
2023-01-26 17:06:11 -0800

    Staged changes and recommit.

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -11,16 +11,19 @@ namespace COMP003A.Assignment2
     {
         static void Main(string[] args)
         {
-            Console.Clear();
+            //Console segment------------------------------------------------------
+            Console.WriteLine("**************************************************"); 
+            Console.WriteLine("Console Section");
+            Console.WriteLine("**************************************************");
             Console.Title = "COMP-003A - Assignment 2";
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("**************************************************");
-            Console.WriteLine("Console Section");
-            Console.WriteLine("**************************************************");
             Console.WriteLine("\n");
+            //---------------------------------------------------------------------
+
+            //String segment-------------------------------------------------------
             Console.WriteLine("**************************************************");
-            Console.WriteLine("String Section                                    ");
+            Console.WriteLine("String Section                                    "); 
             Console.WriteLine("**************************************************");
             Console.Write("Enter First Name: ");
             string firstName = Console.ReadLine();
@@ -30,19 +33,37 @@ namespace COMP003A.Assignment2
             string lastName = Console.ReadLine();
             Console.Write("Enter age in 2023: ");
             string inputAge = Console.ReadLine();
-            int yearBorn = 2023 - Convert.ToInt32(inputAge);
-            Console.WriteLine($"Hello, {firstName} {middleName} {lastName}. You were born in {yearBorn}.");
+            int yearBorn = 2023 - Convert.ToInt32(inputAge);//---------------------<<<|Data type conversion from string to integer.|
+            Console.WriteLine($"Hello, {firstName} {middleName} {lastName}. You were born in {yearBorn}.");//<<<|String uses interpolation.(Starts with $ prior to quotation|
             Console.WriteLine("\n");
+            //---------------------------------------------------------------------
+
+            //Math segment---------------------------------------------------------
             Console.WriteLine("**************************************************");
             Console.WriteLine("Math Section                                      ");
             Console.WriteLine("**************************************************");
             Console.Write("Enter an integer for integer1: ");
-            string inputInteger1 = Console.ReadLine();
-            int integer1 = Convert.ToInt32(inputInteger1);
+            int integer1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter an integer for integer2: ");
-            string inputInteger2 = Console.ReadLine();
-            int integer2 = Convert.ToInt32(inputInteger2);
+            int integer2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"{integer1} + {integer2} = {integer1 + integer2}");
+            Console.WriteLine($"{integer1} - {integer2} = {integer1 - integer2}");
+            Console.WriteLine($"{integer1} * {integer2} = {integer1 * integer2}");
+            Console.WriteLine($"{integer1} / {integer2} = {integer1 / integer2}");
+            Console.WriteLine($"{integer1} % {integer2} = {integer1 % integer2}");
+            Console.WriteLine("\n");
+            //---------------------------------------------------------------------
+
+            //Circle Area Segment--------------------------------------------------
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("Circle Area & Circumference Calculator Section    ");
+            Console.WriteLine("**************************************************");
+            Console.Write("Enter radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+            double circumference = 2 * Math.PI * radius;
+
+
 
         }
     }
